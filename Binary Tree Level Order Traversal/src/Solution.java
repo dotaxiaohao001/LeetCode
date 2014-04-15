bb91bfa27f1fbc2c700b1f1b7e51415a1be46185
PYQ <applepyq@gmail.com>
2014-04-15 04:27:37 -0400

    fuck semicolon

---
+++ Binary Tree Level Order Traversal/src/Solution.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+import java.util.*;
+public class Solution {
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<ArrayList<Integer>>();
+        Queue<TreeNode> q1 = new LinkedList<TreeNode>();
+        Queue<TreeNode> q2 = new LinkedList<TreeNode>();
+        TreeNode p = root;
+        if(p != null)
+        	q1.offer(p);
+        while(q1.peek() != null || q2.peek() != null){
+        	ArrayList<Integer> level1 = new ArrayList<Integer>();
+        	ArrayList<Integer> level2 = new ArrayList<Integer>();
+        	while(q1.peek() != null){
+        		p = q1.poll();
+        		level1.add(p.val);
+        		if(p.left != null)
+        			q2.offer(p.left);
+        		if(p.right != null)//typo more ;
+        			q2.offer(p.right);
+        	}
+        	while(q2.peek() != null){
+        		p = q2.poll();
+        		level2.add(p.val);
+        		if(p.left != null)
+        			q1.offer(p.left);
+        		if(p.right != null)
+        			q1.offer(p.right);
+        	}
+        	if(level1.size() > 0)
+        		arr.add(level1);
+        	if(level2.size() > 0)
+        		arr.add(level2);
+        }
+        return  arr;
+    }
+    public static void main(String args[]){
+    	TreeNode a = new TreeNode(null,null,4);
+    	TreeNode b = new TreeNode(null,null,5);
+    	TreeNode c = new TreeNode(a,null,2);
+    	TreeNode d = new TreeNode(null,b,3);
+    	TreeNode e = new TreeNode(c,d,1);
+    	Solution s = new Solution();
+    	ArrayList<ArrayList<Integer>> arr = s.levelOrder(e);
+    	ArrayList<ArrayList<Integer>> arr1 = arr;
+    }
+}
