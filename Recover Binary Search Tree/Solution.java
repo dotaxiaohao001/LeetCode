f1df4234542386700062832d17f5fa7a97dc06c7
PYQ <applepyq@gmail.com>
2014-07-06 16:09:32 -0400

    recursion should be better

---
+++ Recover Binary Search Tree/Solution.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+	TreeNode pre = null;
+	TreeNode first = null;
+	TreeNode second = null; 
+    public void recoverTree(TreeNode root) {
+        inOrder(root);
+        //swap first and second;
+        int temp = first.val;
+        first.val = second.val;
+        second.val = first.val;
+    }
+
+    private void inOrder(TreeNode root) {
+    	if(root == null)
+    		return;
+    	inOrder(root.left);
+
+    	if(pre != null && pre.val > root.val){
+    		if(first == null){
+    			first = pre; // had find the left position
+    			second = root; // not sure
+    		}else {
+    			second = root;
+    		}
+    	}
+    	pre = root;
+
+    	inOrder(root.right);
+    }
+}
