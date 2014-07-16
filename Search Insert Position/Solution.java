82c1ef781f10a7aec4f0128369eea53461f855f4
PYQ <applepyq@gmail.com>
2014-07-15 21:41:38 -0400

    binary

--- Search Insert Position/Solution.java
+++ Search Insert Position/Solution.java
@@ -13,4 +13,24 @@ public class Solution {
         return i;
 
     }
+
+
+// binary way
+    //    public int searchInsert(int[] A, int target) {
+//        int start = 0;
+//        int end = A.length - 1;
+//
+//        while(start <= end) {
+//            int mid = (start + end) / 2;
+//            if(A[mid] == target)
+//                return mid;
+//            if(A[mid] < target)
+//                start = mid + 1;
+//            else if(A[mid] > target)
+//                end = mid  - 1;
+//        }
+//
+//        return start;
+//
+//    }
 }
