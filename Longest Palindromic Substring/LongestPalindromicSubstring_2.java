b55d0bb63de2ce497fb8f40b291025a3294045f5
PYQ <applepyq@gmail.com>
2014-08-19 00:14:06 -0400

    longset plaidromic substring new

---
+++ Longest Palindromic Substring/LongestPalindromicSubstring_2.java
@@ -0,0 +1,50 @@
+/**
+*Longest Palindromic Substring
+*/
+//mistake:
+//1. typo: paliSub(PaliSub)
+//2. forloop 2nd expression wrong(i < len / i > 0) !!! should be i >= 0
+
+// 思路：
+// 动态规划：
+// 1.用paliSub[i][j] 表示 substring[i,j]是不是palidorme
+// 哈哈哈错误的理解2.更新的时候必须从后往前： 因为1：这样测试的substring length会逐渐增大。直到 len。 如果从前往后难以做到，例如对于1， 开始(0,0), 之后需要(1,1) 然后(0,1)
+// 也可以：从前往后
+//         for(int j = 0; j < len; ++j) {
+//             for(int i = j; i >= 0; --i) {
+public class Solution {
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if(s == null || len < 2)
+        	return s;
+
+        boolean[][] paliSub = new boolean[len][len];
+        //paliSub[i][j] means the substring(i,j) is pali or not
+        for(int i = len-1; i > 0; --i) {
+        	for(int j = i; j < len; ++j) {
+        		//single letter string 
+        		if(i == j)
+        			paliSub[i][j] = true;
+        		//two word
+        		else if(s.charAt(i) == s.charAt(j) && (i+1 == j || paliSub[i+1][j-1]))
+        			paliSub[i][j] = true;
+        		//else is false
+        	}
+        }
+        int max = 0;
+        String result = "";
+        for(int i = 0; i < len; ++i)
+        	for(int j = i; j < len; ++j) {
+        		if(paliSub[i][j] && j+1-i > max) {
+        			max = j-i+1;
+        			result = s.substring(i, j+1);
+        		}
+        	}
+        return result;
+    }
+    
+    public static void main(String[] args) {
+    		Solution s = new Solution();
+    		System.out.println(s.longestPalindrome("acbc"));
+    }
+}
--- Two Sum/TwoSum_2.java
+++ Two Sum/TwoSum_2.java
@@ -6,6 +6,14 @@
 // 2. index is not zero-based should + 1
 // 3. typo， numbers(number), miss ;
 
+
+// 思路：
+// 1.排序数组
+// 2.设置两头reference 从两边 往 中间 move
+// 3.如果sum 为当前l 和 r指向的和
+// 	if sum == target ==>l r为所要求的数
+// 	if sum > target  ==> r-- 所有比r bigger or equal 都 使得 l+r > target 
+// 	if sum < target  ==> l++  同理 < target
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
     	int[] result = new int[2];
